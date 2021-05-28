2c0b5a23c9cc006b60a35884a5731be8668b17f2
Anshul Verma <ansverma@localhost.localdomain>
2021-05-28 06:55:25 +0530

    added validation of number of numbers provided

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,15 @@
 package calc
 
 // returns sum of two integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) error int {
 	sum := 0
 
+	if len(numbers) < 2 {
+		return error.New("Provide more than one number"), sum
+	}
+	
 	for _, num := range numbers {
 		sum = sum + num
 	}
-
-	return sum
+	return nil, sum
 }
